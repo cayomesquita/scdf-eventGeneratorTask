73ac68d9754e1f1f9beac165a21168102282881c
Cayo Amorim <cayomamorim@gmail.com>
2022-01-19 21:35:31 -0800

    First commit

---
+++ src/main/kotlin/org/cayomamorim/scdf/eventGeneratorTask/EventGeneratorTaskApplication.kt
@@ -0,0 +1,11 @@
+package org.cayomamorim.scdf.eventGeneratorTask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EventGeneratorTaskApplication
+
+fun main(args: Array<String>) {
+	runApplication<EventGeneratorTaskApplication>(*args)
+}
---
+++ src/test/kotlin/org/cayomamorim/scdf/eventGeneratorTask/EventGeneratorTaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.cayomamorim.scdf.eventGeneratorTask
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EventGeneratorTaskApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
